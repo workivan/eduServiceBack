a3ee936e87416eb36761e0b757e55804a04eeea5
Ivan Kuzin <ikuzin@deliveryweb.ru>
2021-04-20 10:52:35 +0300

    set platform

--- eduService/settings.py
+++ eduService/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jobkuzin.ru', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'service_auth.CustomUser'
 
@@ -44,13 +44,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'service_auth',
     'core',
-    'corsheaders'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
