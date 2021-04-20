a03d8282b6567493fbb0e0b1dfa263064a3c1741
Ivan Kuzin <ikuzin@deliveryweb.ru>
2021-04-20 10:19:43 +0300

    fix hosts(1)

--- eduService/settings.py
+++ eduService/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jobkuzin.ru', 'localhost:8000']
+ALLOWED_HOSTS = ['jobkuzin.ru', 'localhost']
 
 AUTH_USER_MODEL = 'service_auth.CustomUser'
 
